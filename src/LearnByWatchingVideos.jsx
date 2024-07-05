aa4d8abf749e1e4581a583a8660744706980ddcd
massa medi <managersebowa4525@gmail.com>
2024-07-05 18:17:56 +0300

    I have  just  styled my iframe more and i have deleted the LazyLoad component  compeletly

--- src/LearnByWatchingVideos.jsx
+++ src/LearnByWatchingVideos.jsx
@@ -1,4 +1,3 @@
-import LazyIframe from "./LazyIframe";
 import "./Video.css";
 
 const LearnByWatchingVideos = () => {
@@ -19,109 +18,205 @@ const LearnByWatchingVideos = () => {
 
         <div className="container">
           <div>
-            <LazyIframe
+            <iframe
               src="https://www.youtube.com/embed/GzCm-kVOjwo?si=RDkiWyK-XVbjrbwQ"
               title="YouTube video player"
-            />
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              width="560"
+              height="315"
+            ></iframe>
           </div>
           <div>
-            <LazyIframe
+            <iframe
               src="https://www.youtube.com/embed/qz0aGYrrlhU?si=aLQJUkRkY1B5Ptbc"
               title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              width="560"
+              height="315"
             />
           </div>
           <div>
-            <LazyIframe
+            <iframe
               src="https://www.youtube.com/embed/kUMe1FH4CHE?si=-lKu41UeA99oUEO3"
               title="YouTube video player"
-            />
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              width="560"
+              height="315"
+            ></iframe>
           </div>
           <div>
-            <LazyIframe
+            <iframe
               src="https://www.youtube.com/embed/FQdaUv95mR8?si=9r6dFrqsW4bzz22A"
               title="YouTube video player"
-            />
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              width="560"
+              height="315"
+            ></iframe>
           </div>
         </div>
         <h2>CSS videos</h2>
         <div className="container">
           <div>
-            <LazyIframe
+            <iframe
               src="https://www.youtube.com/embed/OXGznpKZ_sA?si=eY6Jr8pc4_XSyDlA"
               title="YouTube video player"
-            />
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              width="560"
+              height="315"
+            ></iframe>
           </div>
           <div>
-            <LazyIframe
+            <iframe
               src="https://www.youtube.com/embed/wRNinF7YQqQ?si=kGdviv-qFaaGP5Ic"
               title="YouTube video player"
-            />
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              width="560"
+              height="315"
+            ></iframe>
           </div>
           <div>
-            <LazyIframe
+            <iframe
               src="https://www.youtube.com/embed/J5KxiOnIrKg?si=NM7ciCZX4ObaM5xF"
               title="YouTube video player"
-            />
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              width="560"
+              height="315"
+            ></iframe>
           </div>
           <div>
-            <LazyIframe
+            <iframe
               src="https://www.youtube.com/embed/Icf5D3fEKbM?si=RNDcTN_zhzIpG-MJ"
               title="YouTube video player"
-            />
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              width="560"
+              height="315"
+            ></iframe>
           </div>
         </div>
         <h2>Javascript videos</h2>
         <div className="container">
           <div>
-            <LazyIframe
+            <iframe
               src="https://www.youtube.com/embed/8dWL3wF_OMw?si=cb18oo_yL_T5L0-t"
               title="YouTube video player"
-            />
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              width="560"
+              height="315"
+            ></iframe>
           </div>
           <div>
-            <LazyIframe
+            <iframe
               src="https://www.youtube.com/embed/PkZNo7MFNFg?si=4aYJEikCuqDF0C9r"
               title="YouTube video player"
-            />
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              width="560"
+              height="315"
+            ></iframe>
           </div>
           <div>
-            <LazyIframe
+            <iframe
               src="https://www.youtube.com/embed/XIOLqoPHCJ4?si=hbmAJDPe9llqm59_"
               title="YouTube video player"
-            />
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              width="560"
+              height="315"
+            ></iframe>
           </div>
           <div>
-            <LazyIframe
+            <iframe
               src="https://www.youtube.com/embed/xc3a_CJhjCc?si=bg09RPwTjxIMwpyt"
               title="YouTube video player"
-            />
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              width="560"
+              height="315"
+            ></iframe>
           </div>
         </div>
         <h2>React.js videos</h2>
         <div className="container">
           <div>
-            <LazyIframe
+            <iframe
               src="https://www.youtube.com/embed/cd3P3yXyx30?si=4WRMxvFSV5O0gs_Q"
               title="YouTube video player"
-            />
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              width="560"
+              height="315"
+            ></iframe>
           </div>
           <div>
-            <LazyIframe
+            <iframe
               src="https://www.youtube.com/embed/2-crBg6wpp0?si=mcW8G-H-NIyc3EIq"
               title="YouTube video player"
-            />
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              width="560"
+              height="315"
+            ></iframe>
           </div>
           <div>
-            <LazyIframe
+            <iframe
               src="https://www.youtube.com/embed/LDB4uaJ87e0?si=QdqcS_UPuQWQgzbX"
               title="YouTube video player"
-            />
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              width="560"
+              height="315"
+            ></iframe>
           </div>
           <div>
-            <LazyIframe
+            <iframe
               src="https://www.youtube.com/embed/4DqAvWonPAg?si=i45zGcqDw-cPvdcJ"
               title="YouTube video player"
-            />
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+              width="560"
+              height="315"
+            ></iframe>
           </div>
         </div>
       </div>
