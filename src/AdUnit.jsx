cb61f35205b9bc2286c964fd1b0c5586c1711c49
massa medi <managersebowa4525@gmail.com>
2024-07-17 20:41:37 +0300

    Im have  just  improved  on the  adUnit component and also  i  the  css  code is  modiied

--- src/AdUnit.jsx
+++ src/AdUnit.jsx
@@ -1,26 +1,3 @@
-// const AdUnit = () => {
-//   return (
-//     <div className="display-ad">
-//       <script
-//         async
-//         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6271278895910760"
-//         crossorigin="anonymous"
-//       ></script>
-//       <ins
-//         className="adsbygoogle"
-//         style={{ display: "block" }}
-//         data-ad-client="ca-pub-6271278895910760"
-//         data-ad-slot="8956915466"
-//         data-ad-format="auto"
-//         data-full-width-responsive="true"
-//       />
-//       <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-//     </div>
-//   );
-// };
-
-// export default AdUnit;
-
 import React, { useEffect } from "react";
 
 const AdUnit = () => {
@@ -42,16 +19,14 @@ const AdUnit = () => {
   }, []);
 
   return (
-    <div className="display-ad">
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-client="ca-pub-6271278895910760"
-        data-ad-slot="8956915466"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      />
-    </div>
+    <ins
+      className="adsbygoogle"
+      style={{ display: "block" }}
+      data-ad-client="ca-pub-6271278895910760"
+      data-ad-slot="8956915466"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    />
   );
 };
 
