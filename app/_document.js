0573353d51967e68f901e89b1c6685088ef94976
massa medi <managersebowa4525@gmail.com>
2024-09-30 16:33:07 +0300

    I  have done nothing

--- app/_document.js
+++ app/_document.js
@@ -1,89 +1,85 @@
 // pages/_document.js
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          {/* Google Tag Manager */}
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-EVC3DTG6XZ"
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-EVC3DTG6XZ');
-              `,
-            }}
-          />
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google Analytics */}
+        <Script
+          strategy="beforeInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-EVC3DTG6XZ"
+        />
+        <Script
+          id="google-analytics"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-EVC3DTG6XZ');
+            `,
+          }}
+        />
 
-          {/* Google AdSense */}
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6271278895910760"
-            crossOrigin="anonymous"
-          />
+        {/* Google AdSense */}
+        <Script
+          async
+          strategy="beforeInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6271278895910760"
+          crossOrigin="anonymous"
+        />
 
-          {/* Clicky Analytics */}
-          <script async data-id="101458649" src="//static.getclicky.com/js" />
+        {/* Clicky Analytics */}
+        <Script
+          async
+          strategy="beforeInteractive"
+          data-id="101458649"
+          src="//static.getclicky.com/js"
+        />
 
-          {/* Structured Data */}
-          <script
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{
-              __html: JSON.stringify({
-                "@context": "https://schema.org",
-                "@type": "BreadcrumbList",
-                itemListElement: [
-                  {
-                    "@type": "ListItem",
-                    position: 1,
-                    name: "Home",
-                    item: "https://www.mergesociety.com/",
-                  },
-                  {
-                    "@type": "ListItem",
-                    position: 2,
-                    name: "Tutorials",
-                    item: "https://www.mergesociety.com/tutorials",
-                  },
-                ],
-              }),
-            }}
-          />
+        {/* Structured Data */}
+        <Script
+          id="structured-data"
+          type="application/ld+json"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "BreadcrumbList",
+              itemListElement: [
+                {
+                  "@type": "ListItem",
+                  position: 1,
+                  name: "Home",
+                  item: "https://www.mergesociety.com/",
+                },
+                {
+                  "@type": "ListItem",
+                  position: 2,
+                  name: "Tutorials",
+                  item: "https://www.mergesociety.com/tutorials",
+                },
+              ],
+            }),
+          }}
+        />
 
-          {/* Grow.me Script */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                !(function(){
-                  window.growMe || ((window.growMe = function(e){
-                    window.growMe._.push(e);
-                  }), (window.growMe._ = []));
-                  var e = document.createElement("script");
-                  e.type = "text/javascript";
-                  e.src = "https://faves.grow.me/main.js";
-                  e.defer = !0;
-                  e.setAttribute("data-grow-faves-site-id", "U2l0ZTpjZjVmMjdhMy1kNTQwLTQwZWEtYjBhOC1mMzA0MTgzMTRkNWQ=");
-                  var t = document.getElementsByTagName("script")[0];
-                  t.parentNode.insertBefore(e, t);
-                })();
-              `,
-            }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+        {/* Grow Me Script */}
+        <Script
+          id="grow-me"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `!(function(){window.growMe||(window.growMe=function(e){window.growMe._.push(e);},window.growMe._=[]);var e=document.createElement("script");e.type="text/javascript",e.src="https://faves.grow.me/main.js",e.defer=!0,e.setAttribute("data-grow-faves-site-id","U2l0ZTpjZjVmMjdhMy1kNTQwLTQwZWEtYjBhOC1mMzA0MTgzMTRkNWQ=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();`,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
