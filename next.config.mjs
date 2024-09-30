f82259c52956c307db38d5d7f70ea974e8b67304
massa medi <managersebowa4525@gmail.com>
2024-09-30 12:15:43 +0300

    disk  deeted

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "dist",
+  // distDir: "dist",
   images: {
     domains: ["res.cloudinary.com", "www.freecodecamp.org"],
   },
