15c98f11e6be97bcf134ee88d19cb86034b2b0f1
massa medi <managersebowa4525@gmail.com>
2024-09-30 14:31:44 +0300

    I have  added the ad  unit and am  gonna test  it

--- app/AdUnit.js
+++ app/AdUnit.js
@@ -1,67 +1,41 @@
 "use client";
 
-// import React, { useEffect } from "react";
-
-// const withAds = (WrappedComponent, adClient, adSlot) => {
-//   const AdComponent = (props) => {
-//     useEffect(() => {
-//       const script = document.createElement("script");
-//       script.src =
-//         "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-//       script.async = true;
-//       script.crossOrigin = "anonymous";
-//       document.head.appendChild(script);
-
-//       script.onload = () => {
-//         (window.adsbygoogle = window.adsbygoogle || []).push({});
-//       };
-
-//       return () => {
-//         document.head.removeChild(script);
-//       };
-//     }, []);
-
-//     return (
-//       <div>
-//         <WrappedComponent {...props} />
-//         <ins
-//           className="adsbygoogle"
-//           style={{ display: "block" }}
-//           data-ad-client={adClient}
-//           data-ad-slot={adSlot}
-//           data-ad-format="auto"
-//           data-full-width-responsive="true"
-//         />
-//         <br />
-//       </div>
-//     );
-//   };
-
-//   return AdComponent;
-// };
-
-// export default withAds;
-
 // components/AdUnit.js
-import React, { useEffect } from "react";
+
+import { useEffect, useRef } from "react";
 
 const AdUnit = () => {
+  const adLoaded = useRef(false); // Ref to track if the ad has been loaded
+
   useEffect(() => {
-    (window.adsbygoogle = window.adsbygoogle || []).push({});
+    const script = document.createElement("script");
+    script.src =
+      "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6271278895910760";
+    script.async = true;
+    script.crossOrigin = "anonymous";
+    document.body.appendChild(script);
+
+    // Push ad only if not already loaded
+    if (!adLoaded.current) {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+      adLoaded.current = true; // Mark as loaded
+    }
+
+    // Clean up the script on component unmount
+    return () => {
+      document.body.removeChild(script);
+    };
   }, []);
 
   return (
-    <div>
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-client="ca-pub-6271278895910760"
-        data-ad-slot="8956915466"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      />
-      <br />
-    </div>
+    <ins
+      className="adsbygoogle"
+      style={{ display: "block" }}
+      data-ad-client="ca-pub-6271278895910760"
+      data-ad-slot="8956915466"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
   );
 };
 
--- app/_document.js
+++ app/_document.js
@@ -1,55 +1,39 @@
 // pages/_document.js
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { Montserrat, Roboto } from "next/font/google";
-
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  variable: "--font-montserrat",
-  display: "swap",
-  weight: ["400", "500", "700"],
-});
-
-const roboto = Roboto({
-  subsets: ["latin"],
-  variable: "--font-roboto",
-  display: "swap",
-  weight: ["400", "500", "700"],
-});
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html>
         <Head>
+          {/* Google Tag Manager */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-EVC3DTG6XZ"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-EVC3DTG6XZ');
+              `,
+            }}
+          />
+
           {/* Google AdSense */}
           <script
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6271278895910760"
             crossOrigin="anonymous"
-          ></script>
-
-          {/* Google Tag Manager */}
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-EVC3DTG6XZ"
-          ></script>
-          <script id="gtag-init">
-            {`window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-EVC3DTG6XZ');`}
-          </script>
+          />
 
           {/* Clicky Analytics */}
-          <script
-            async
-            data-id="101458649"
-            src="//static.getclicky.com/js"
-          ></script>
+          <script async data-id="101458649" src="//static.getclicky.com/js" />
 
           {/* Structured Data */}
           <script
-            id="structured-data"
             type="application/ld+json"
             dangerouslySetInnerHTML={{
               __html: JSON.stringify({
@@ -72,8 +56,28 @@ class MyDocument extends Document {
               }),
             }}
           />
+
+          {/* Grow.me Script */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !(function(){
+                  window.growMe || ((window.growMe = function(e){
+                    window.growMe._.push(e);
+                  }), (window.growMe._ = []));
+                  var e = document.createElement("script");
+                  e.type = "text/javascript";
+                  e.src = "https://faves.grow.me/main.js";
+                  e.defer = !0;
+                  e.setAttribute("data-grow-faves-site-id", "U2l0ZTpjZjVmMjdhMy1kNTQwLTQwZWEtYjBhOC1mMzA0MTgzMTRkNWQ=");
+                  var t = document.getElementsByTagName("script")[0];
+                  t.parentNode.insertBefore(e, t);
+                })();
+              `,
+            }}
+          />
         </Head>
-        <body className={`${montserrat.variable} ${roboto.variable}`}>
+        <body>
           <Main />
           <NextScript />
         </body>
