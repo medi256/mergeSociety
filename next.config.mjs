6faaad90832a1cccd4a7e265b07b6e7ad189b813
massa medi <managersebowa4525@gmail.com>
2024-09-30 11:19:31 +0300

    I have just  changed or  improved  my  files in next  config  to  deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: "dist", // Explicitly set output directory
   images: {
     domains: ["res.cloudinary.com", "www.freecodecamp.org"],
   },
   eslint: {
-    ignoreDuringBuilds: true, // Add this line
+    ignoreDuringBuilds: true,
   },
 };
 
