c406e24044a96fd3e4046014c7ef9a2d4ea4674e
massa medi <managersebowa4525@gmail.com>
2024-11-06 07:30:00 +0300

    I have added the grow  ad  in my script  layout

--- app/layout.js
+++ app/layout.js
@@ -113,6 +113,15 @@ export default function RootLayout({ children }) {
             !(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTpjZjVmMjdhMy1kNTQwLTQwZWEtYjBhOC1mMzA0MTgzMTRkNWQ=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();
           `}
         </Script>
+
+        {/* Add the Journey monetization script */}
+        <Script
+          src="//scripts.scriptwrapper.com/tags/cf5f27a3-d540-40ea-b0a8-f30418314d5d.js"
+          async
+          data-noptimize="1"
+          data-cfasync="false"
+          strategy="afterInteractive"
+        />
       </head>
       <body className={`${montserrat.variable} ${roboto.variable}`}>
         <NavBar />
