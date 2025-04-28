40850e53c785555c810002904fc2a5223502b422
massa medi <managersebowa4525@gmail.com>
2025-04-28 12:28:04 +0300

    I am solving this  eslint error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,13 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
+];
 
 export default eslintConfig;
